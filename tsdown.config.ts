19d47ecf3b78270246381035184ef046e6f6936d
Stepan Samko <step.samko@gmail.com>
2026-02-06 00:42:53 +0200

    chore: project scaffolding with tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+    cli: 'src/cli/index.ts',
+  },
+  format: ['esm', 'cjs'],
+  dts: true,
+  platform: 'node',
+  clean: true,
+  fixedExtension: false,
+})
